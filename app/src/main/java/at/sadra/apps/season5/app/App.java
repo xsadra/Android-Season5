a1b8ea39a82de31cda5d9d65204c7dd874a0fa4c
Sadra <31988724+xsadra@users.noreply.github.com>
2018-09-22 19:21:48 +0200

    Add App Class

---
+++ app/src/main/java/at/sadra/apps/season5/app/App.java
@@ -0,0 +1,21 @@
+package at.sadra.apps.season5.app;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class App {
+
+    public static class Main {
+        public static final String TAG = "Season5";
+    }
+
+    public static void log(String message) {
+        Log.e(Main.TAG, message);
+    }
+
+    public static void toast(String message) {
+        Toast
+                .makeText(Application.getContext(), message, Toast.LENGTH_LONG)
+                .show();
+    }
+}
